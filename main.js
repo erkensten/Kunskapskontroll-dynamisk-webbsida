60c6b15fe477d25ee3269c6be2c7df6970eb7271
Mattis Erkensten <mattis.erkensten@cmeducations.se>
2020-12-10 15:37:20 +0100

    Javascript-type-effect-added

--- main.js
+++ main.js
@@ -7,9 +7,37 @@ hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('show');
 });
 
+// Typwriter H1 Animation 
+
+const texts = ['Webbanalyser', 'Sökordsanalyser', 'Teknisk Optimering'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 300);
+
+}());
+
 // Mouse animation
 
 const mouseAnimation = document.getElementById('mouse-animation');
+
 
 mouseAnimation
 
