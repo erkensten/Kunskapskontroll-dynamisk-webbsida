41dd8aefcc678c553f095bbf20bc8fa81a62d4d4
Mattis Erkensten <mattis.erkensten@cmeducations.se>
2020-12-10 14:52:31 +0100

    added-js

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-const text = document.querySelector('.h1-text');
-console.log(text);
+// Hamburger menu 
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileNav = document.getElementById('mobile-nav');
+
+hamburgerMenu.addEventListener('click', () => {
+    mobileNav.classList.toggle('show');
+});
+
+// Mouse animation
+
+const mouseAnimation = document.getElementById('mouse-animation');
+
+mouseAnimation
+
+
+// Scroll to top button
+
+const btnScrollToTop = document.getElementById('btnScrollToTop');
+
+btnScrollToTop.addEventListener('click', function () {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+});
